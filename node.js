bf61ca8451f327c6d95712881d3f4f72acea35e1
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2023-10-15 15:35:22 +0200

    npm

--- node.js
+++ node.js
@@ -1,1 +1,17 @@
 const express = require("express");
+const app = express();
+const ejs = require("ejs")
+
+app.set("view engine","ejs");
+app.use(express.static("views"));
+
+
+app.get("/",(req,res)=>{
+    res.render("Emoyeni");
+});
+
+
+
+app.listen(7600, ()=> {
+    console.log("Danko Supreme");
+});
